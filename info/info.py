ca1e6e28e4c4a59f21e1549ecc3c4bc1e61200f2
Wolf <fw56moba@idiv.de>
2016-08-11 10:59:43 +0200

    change in write raster

--- info/info.py
+++ info/info.py
@@ -4,11 +4,13 @@ from datetime import datetime
 import  socket
 from glob import glob
 import os
+import fnmatch
+
 
 
 
 def Help(inhal = ''):
-    HelpInhalt =  sorted(['timestr', 'time', 'hostname', 'folderCount','getsizeMB'])
+    HelpInhalt =  sorted(['timestr', 'time', 'hostname', 'folderCount','getsizeMB', 'glob_rec'])
     if inhal =='':
         inhal = HelpInhalt
     inhalt = inhal
@@ -56,18 +58,18 @@ def Help(inhal = ''):
             "folderCount" :""" folderCount
                 returns the number of Folders in the directory
 
-		>>>folderCount(fillpath, wildcard= "*",position = "m")
-		
-		fullpath --> path to the folder
-		wildcard --> if not set, it will use '*' as default
-		position --> to specify the wildcard 
-			     by default m --> '*'
-			     options:
-			     	s --> startswith 'name*'
-				e --> endswith '*name'
-		if you just want to count tiffs:
-		folderCount(path,'.tiff','e')
-		--> returns the count of all tiffs in the folder
+        >>>folderCount(fillpath, wildcard= "*",position = "m")
+        
+        fullpath --> path to the folder
+        wildcard --> if not set, it will use '*' as default
+        position --> to specify the wildcard 
+                 by default m --> '*'
+                 options:
+                     s --> startswith 'name*'
+                e --> endswith '*name'
+        if you just want to count tiffs:
+        folderCount(path,'.tiff','e')
+        --> returns the count of all tiffs in the folder
 
  _______________________________________________________________________________
             """,
@@ -75,13 +77,24 @@ def Help(inhal = ''):
             "getsizeMB":""" getsizeMB:  
                 raturns the size of a file or folder in MB
 
-		>>>getsizeMB(fullpath)
+        >>>getsizeMB(fullpath)
 
-		fullpath --> full path to file or folder
+        fullpath --> full path to file or folder
                 
  _______________________________________________________________________________
-            """
+            """,
             
+            "glob_rec":""" glob_rec:
+                returns a list of all files in the path which match
+                wildcard option
+                
+        >>files=glob_rec(fullpath,'wildcard')
+        
+        fullpath --> full path to folder
+        wildcard --> '*something*else*.tif' has to have the *
+        
+_______________________________________________________________________________
+            """            
             }
             
     print myDic["header"]
@@ -110,41 +123,50 @@ def Help(inhal = ''):
 
 
 def timestr():
-	""" returns the actual time as a string -> HH:MM:SS"""
-	ti = datetime.now().strftime("%H:%M:%S")
-	return ti
+    """ returns the actual time as a string -> HH:MM:SS"""
+    ti = datetime.now().strftime("%H:%M:%S")
+    return ti
 
 def time():
-	ti = datatime.now()
-	return ti
+    ti = datatime.now()
+    return ti
 
 def hostname():
-	""" returns the actual host where the script is running"""
-	host = socket.gethostname()
-	return host
+    """ returns the actual host where the script is running"""
+    host = socket.gethostname()
+    return host
 
 
 def folderCount(path, wildcard= "*",position = "m"):
-	""" 
-	retuns the count of folders or files in a folder
+    """ 
+    retuns the count of folders or files in a folder
 
-	with no given wildcard it uses * 
-	with given wildcard the default is "*wildcard*"
-	if you need to specify the wildcard you can use:
-	      s for wildcard starts with --> str*  or  
-	      e for wildcard ends with	 --> *.tif 
-	"""
-	if path[-1] != "/" or path[-1]!="\\":
-		path = path+"/"
-	if position == "m":
-		wildcard = "*"+wildcard+"*"
-	elif position == "s":
-		wildcard = wildcard+"*"
-	elif position == "e":
-		wildcard == "*"+wildcard
-	count = len(glob(path+wildcard))
-	return count
-		
+    with no given wildcard it uses * 
+    with given wildcard the default is "*wildcard*"
+    if you need to specify the wildcard you can use:
+          s for wildcard starts with --> str*  or  
+          e for wildcard ends with     --> *.tif 
+    """
+    if path[-1] != "/" or path[-1]!="\\":
+        path = path+"/"
+    if position == "m":
+        wildcard = "*"+wildcard+"*"
+    elif position == "s":
+        wildcard = wildcard+"*"
+    elif position == "e":
+        wildcard == "*"+wildcard
+    count = len(glob(path+wildcard))
+    return count
+        
 def getsizeMB(fullpath):
     size = ("%.2f" % (os.path.getsize(fullpath)/1024/1024.0))
     return size
+
+    
+def glob_rec(path,wildcard):
+    """works like glob just with the recursive option"""
+    matches = []
+    for root, dirnames, filenames in os.walk(path):
+        for filename in fnmatch.filter(filenames, wildcard):
+            matches.append(os.path.join(root, filename))
+    return matches
