4624a1762f3209b967cbd6922dbcd67344d61aa2
Florian Wolf <florian.wolf@idiv.de>
2018-01-23 18:03:40 +0100

    added comments

--- __init__.py
+++ __init__.py
@@ -91,20 +91,26 @@ def README():
     print myDic["header"]
     
 def outname(path, new_name_part = False , extension = False):
-    no_change = path[:path.rfind('.')]
-    if new_name_part == False and extension == False:
+    '''this function is to rename / create output name for files
+        the new_name_part will be added to the orig filename, 
+        the extension is the new, if wanted
+        if not, its the same like the input
+        new_name_part or extension has to be given'''
+        
+    no_change = path[:path.rfind('.')] #split on last point in string
+    if new_name_part == False and extension == False: #checjk if new_name_part and extension are give
         print 'error in naming, input is the same as output'
         return False
-    if extension == False:
+    if extension == False: #if no extension is given, take the on from the input file
         extension = path[path.rfind('.'):]
-    if extension.startswith('.'):
+    if extension.startswith('.'): #remove . from the extension if given
         extension = extension[1:]
-    if new_name_part == False:
+    if new_name_part == False: #if new_name_part is not given assign empty string to the variable
         new_name_part = ''
-    if new_name_part.startswith('_'):
+    if new_name_part.startswith('_'): #if given check if it starts with _ if yes, remove it
         new_name_part = new_name_part[1:]
-    outname = no_change+'_'+new_name_part+'.'+extension
-    if path == outname:
+    outname = no_change+'_'+new_name_part+'.'+extension #put all things together
+    if path == outname: #check if new outname is equal to input
         print 'error in naming, input is the same as output'
         return False
-    return outname
+    return outname #return the new name
