a5f56f5320b8fcceb9fe9437758b8895259ac032
Florian Wolf <florian.wolf@idiv.de>
2018-07-18 11:14:06 +0200

    cleanup comments and def description

--- info/info.py
+++ info/info.py
@@ -1,125 +1,25 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 02 19:15:18 2016
 
-from datetime import datetime
-import  socket
-from glob import glob
-import os, sys
-import fnmatch
+@author: Florian Wolf
 
 
-
-
-def Help(inhal = ''):
-    HelpInhalt =  sorted(['timestr', 'time', 'hostname', 'folderCount','getsizeMB', 'glob_rec'])
-    if inhal =='':
-        inhal = HelpInhalt
-    inhalt = inhal
-    if type(inhalt)== str:
-        cList = []
-        cList.append(inhalt)
-        cList.append("nix")
-        inhalt = cList
-
-    myDic = {"header": """
             __________________________________________________
-            ###               MacPyver.info                ###
+            ###              MacPyver.info                 ###
             ###   The Swissknife like Python-Package for   ###
             ###        work in general and with GIS        ###
             __________________________________________________
 
-                How to use the functions:
-                __________
+"""
 
-             """,
 
-            "timestr":""" timestr:
-                returns the time as a string
+from datetime import datetime
+import socket
+from glob import glob
+import os
+import fnmatch
 
-                >>> timestr()
-
- _______________________________________________________________________________
-            """,
-
-            "time":""" time:
-                returns the time to calculate
-
-                >>> time()
-
- _______________________________________________________________________________
-            """,
-
-            "hostname":""" hostname:
-                returns the hostname where you are running the Python
-
-                >>>hostname()
-
- _______________________________________________________________________________
-            """,
-            "folderCount" :""" folderCount
-                returns the number of Folders in the directory
-
-        >>>folderCount(fillpath, wildcard= "*",position = "m")
-
-        fullpath --> path to the folder
-        wildcard --> if not set, it will use '*' as default
-        position --> to specify the wildcard
-                 by default m --> '*'
-                 options:
-                     s --> startswith 'name*'
-                e --> endswith '*name'
-        if you just want to count tiffs:
-        folderCount(path,'.tiff','e')
-        --> returns the count of all tiffs in the folder
-
- _______________________________________________________________________________
-            """,
-
-            "getsizeMB":""" getsizeMB:
-                raturns the size of a file or folder in MB
-
-        >>>getsizeMB(fullpath)
-
-        fullpath --> full path to file or folder
-
- _______________________________________________________________________________
-            """,
-
-            "glob_rec":""" glob_rec:
-                returns a list of all files in the path which match
-                wildcard option default wildcard is: '*'
-
-        >>files=glob_rec(fullpath,'wildcard')
-
-        fullpath --> full path to folder
-        wildcard --> '*something*else*.tif' has to have the *
-
-_______________________________________________________________________________
-            """
-            }
-
-    print myDic["header"]
-    counter = 0
-    inhalt.sort()
-    op = []
-    for ele in inhalt:
-        for el in myDic.keys():
-            if ele.lower() in el.lower():
-                op.append(el)
-                #print myDic[el]
-                counter += 1
-
-    if counter >0:
-        op = sorted(list(set(op)))
-        for ele in op:
-            print myDic[ele]
-    elif counter == 0:
-        print "     >>> Error: no function with %r <<<" % inhal
-        print ""
-        print "     all available function: "
-        for ele in HelpInhalt:
-            print myDic[ele]
-
-'''________________________________END-DIC______________________________________'''
 
 
 def timestr():
@@ -128,6 +28,7 @@ def timestr():
     return ti
 
 def time():
+    '''returns the actual time as time object'''
     ti = datetime.now()
     return ti
 
@@ -141,11 +42,18 @@ def folderCount(path, wildcard= "*",position = "m"):
     """ 
     retuns the count of folders or files in a folder
 
-    with no given wildcard it uses *
-    with given wildcard the default is "*wildcard*"
-    if you need to specify the wildcard you can use:
-          s for wildcard starts with --> str*  or
-          e for wildcard ends with     --> *.tif
+        >>>folderCount(fillpath, wildcard= "*",position = "m")
+
+        fullpath --> path to the folder
+        wildcard --> if not set, it will use '*' as default
+        position --> to specify the wildcard
+                 by default m --> '*'xxx'*'
+                 options:
+                s --> startswith 'name*'
+                e --> endswith '*name'
+        if you just want to count tiffs:
+        folderCount(path,'.tiff','e')
+        --> returns the count of all tiffs in the folder
     """
     if path[-1] != "/" or path[-1]!="\\":
         path = path+"/"
@@ -159,6 +67,12 @@ def folderCount(path, wildcard= "*",position = "m"):
     return count
 
 def getsizeMB(fullpath):
+    '''
+     returns the size of a file or folder in MB
+        >>>getsizeMB(fullpath)
+
+        fullpath --> full path to file or folder 
+    '''
     size = ("%.2f" % (os.path.getsize(fullpath)/1024/1024.0))
     return size
 
@@ -221,6 +135,13 @@ crctab = [ 0x00000000, 0x04c11db7, 0x09823b6e, 0x0d4326d9, 0x130476dc,
 UNSIGNED = lambda n: n & 0xffffffff
 
 def cksum(path):
+    '''
+    calculates the cksum (checksum) of the given file
+    
+    >>> cksum(fullpath)
+    
+    returns the cksum
+    '''
     b = open(path,'rb').read()
     n = len(b)
     i = c = s = 0
