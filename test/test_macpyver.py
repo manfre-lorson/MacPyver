97b8979955667b1f2860dd27507e7db4f7d84806
Florian Wolf <florian.wolf@idiv.de>
2019-02-08 15:54:38 +0100

    added test

--- test/test_macpyver.py
+++ test/test_macpyver.py
@@ -4,3 +4,6 @@ import MacPyver as mp
 def outname():
     assert mp.__file__[:-4]+'_test.tif' = mp.outname(mp.__file__, 'test','tif')
 
+
+def test_glob_rec():
+    assert len(mp.info.info.glob_rec(mp.__file__[:-13])) == 505
